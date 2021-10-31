c088cb83b92024d9deba0580da0d4129130144b6
nahtand <nathando13403@gmail.com>
2021-10-30 23:56:43 -0700

    files that i may or may not use

---
+++ src/Hitbox.java
@@ -0,0 +1,12 @@
+public class Hitbox {
+    int x, y;
+    int width, height;
+
+    public Hitbox(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
+
